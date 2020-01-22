b16be63349c45006594344f594811c7eccbdac9e
Ajibola Salami <babagee500@gmail.com>
2020-01-22 18:06:33 +0100

    Visualization query for question 2

--- vizqueries.sql
+++ vizqueries.sql
@@ -22,3 +22,28 @@ WHERE c.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music
 ON T2.title = T3.title
 ORDER BY 2, 1) AS T4
 GROUP BY 1;
+
+
+
+/*VISUALIZATION QUERY 2: TO OBTAIN THE AVERAGE LENGTH OF RENTAL DURATION FOR ALL MOVIES AND FAMILY-FRIENDLY MOVIES*/
+SELECT T3.all_movies, T4.family_movies
+FROM (SELECT name, AVG (rental_duration) OVER()         AS all_movies
+      FROM (SELECT f.title, c.name, f.rental_duration, NTILE(4) OVER (PARTITION BY c.name ORDER BY rental_duration) AS standard_quartile
+	        FROM film f
+	  		JOIN film_category fc
+	  		ON f.film_id = fc.film_id
+	  		JOIN category c
+	  		ON c.category_id = fc.category_id) AS T1)   AS T3
+
+JOIN (SELECT name, AVG (rental_duration) OVER() 	  AS family_movies
+	  FROM (SELECT f.title, c.name, f.rental_duration, NTILE(4) OVER (PARTITION BY c.name ORDER BY rental_duration) AS standard_quartile
+			FROM film f
+			JOIN film_category fc
+			ON f.film_id = fc.film_id
+			JOIN category c
+			ON c.category_id = fc.category_id
+     		WHERE name  IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')) AS T2) 
+      AS T4
+ON T3.name = T4.name
+LIMIT 1
+
