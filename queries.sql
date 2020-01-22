93bb2534381e5b882619e3c9bf6dd179540a021e
Ajibola Salami <babagee500@gmail.com>
2020-01-22 17:22:24 +0100

    Answer query for question 2

--- queries.sql
+++ queries.sql
@@ -44,3 +44,42 @@ JOIN
 ON T2.title = T3.title
 ORDER BY 2, 1;
 
+
+
+**SET1 QUESTION 2**
+
+/*QUERY 2.1: TO OBTAIN THE QUARTILE OF THE RENTAL DURATION OF EACH FILM (REQUIRED TABLE)*/
+SELECT f.title, c.name, f.rental_duration, NTILE(4) OVER (ORDER BY rental_duration) AS standard_quartile
+FROM film f
+JOIN film_category fc
+ON f.film_id = fc.film_id
+JOIN category c
+ON c.category_id = fc.category_id
+
+/*QUERY 2.2: TO OBTAIN THE AVERAGE RENTAL DURATION OF ALL FILMS*/
+SELECT AVG (rental_duration) AS avg_rental_overall
+FROM     (SELECT f.title, c.name, f.rental_duration, NTILE(4) OVER (PARTITION BY c.name ORDER BY rental_duration) AS standard_quartile
+          FROM film f
+	  JOIN film_category fc
+	  ON f.film_id = fc.film_id
+	  JOIN category c
+	  ON c.category_id = fc.category_id) AS T1
+
+/*QUERY 2.3: TO OBTAIN THE QUARTILE OF THE RENTAL DURATION OF FAMILY-FRIENDLY FILMS*/
+SELECT f.title, c.name, f.rental_duration, NTILE(4) OVER (PARTITION BY c.name ORDER BY rental_duration) AS standard_quartile
+FROM film f
+JOIN film_category fc
+ON f.film_id = fc.film_id
+JOIN category c
+ON c.category_id = fc.category_id
+WHERE name  IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
+
+/*QUERY 2.4: TO OBTAIN THE AVERAGE RENTAL DURATION OF FAMILY-FRIENDLY FILMS*/
+SELECT AVG (rental_duration) AS avg_duration_family_movies
+FROM   	   (SELECT f.title, c.name, f.rental_duration, NTILE(4) OVER (PARTITION BY c.name ORDER BY rental_duration) AS standard_quartile
+	    FROM film f
+	    JOIN film_category fc
+	    ON f.film_id = fc.film_id
+	    JOIN category c
+	    ON c.category_id = fc.category_id
+     	    WHERE name  IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')) AS T2
